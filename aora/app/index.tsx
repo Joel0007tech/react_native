7de69da3a9b8846d4b5d4f9cd0e821a6c93bd0c0
Joel <ajiolajoel@gmail.com>
2024-08-03 20:16:23 +0100

    update to my web app project

--- aora/app/index.tsx
+++ aora/app/index.tsx
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function App(){
     return(
-        <View style={styles.container}>
-            <Text> Aora!</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl font-black"> Aora web app!</Text>
             <StatusBar style="auto" />
             <Link href="/Profile" style={{color: 'blue'}}> Go to profile page </Link>
         </View>
-    )
+    );
 }
-
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor:'#ffff',
-        alignItems:'center',
-        justifyContent:'center'
-    }
-})
