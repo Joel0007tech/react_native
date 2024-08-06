dcfc1563376bd9dd76dafd4bbf1d4f90f88e3ccb
Joel <ajiolajoel@gmail.com>
2024-08-06 13:43:09 +0100

    update to web app

--- aora/app/index.tsx
+++ aora/app/index.tsx
@@ -7,7 +7,7 @@ export default function App(){
         <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-3xl font-black"> Aora web app!</Text>
             <StatusBar style="auto" />
-            <Link href="/Profile" style={{color: 'blue'}}> Go to profile page </Link>
+            <Link href="/(tabs)home" style={{color: 'blue'}}> Go to Home</Link>
         </View>
     );
 }
