10d87e9e37c480868c0cf043a3c8eef183f9ee1a
Joel <ajiolajoel@gmail.com>
2024-08-08 19:52:07 +0100

    update to web app rn

--- aora/app/search/[query].jsx
+++ aora/app/search/[query].jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Query = () => {
+const Search = () => {
   return (
     <View>
-      <Text>Query</Text>
+      <Text>Search</Text>
     </View>
   )
 }
 
-export default Query
+export default Search
